4fdf7028680ced9eef06551521c1d74af46c9f4b
tlethanawit321 <48791751+tlethanawit321@users.noreply.github.com>
2020-10-22 23:33:11 +0700

    assign any user to role

--- console/migrations/m201022_150734_create_assignment_of_post.php
+++ console/migrations/m201022_150734_create_assignment_of_post.php
@@ -18,10 +18,10 @@ class m201022_150734_create_assignment_of_post extends Migration
         $admin= $auth->getRole('admin');
         $superadmin= $auth->getRole('super-admin');
 
-
         $auth->assign($author, 1);
-        $auth->assign($superadmin, 2);
-
+        $auth->assign($admin, 2);
+        $auth->assign($superadmin, 3);
+      
         return true;
     }
 
@@ -36,7 +36,9 @@ class m201022_150734_create_assignment_of_post extends Migration
         $admin= $auth->getRole('admin');
         $superadmin= $auth->getRole('super-admin');
 
+        $auth->revoke($author, 1);
         $auth->revoke($admin, 2);
+        $auth->revoke($superadmin, 3);
         
         return true;
     }
