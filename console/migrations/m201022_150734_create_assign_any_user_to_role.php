d03f62ad185a5ed028922e3cf72ff4b515e1b682
tlethanawit321 <48791751+tlethanawit321@users.noreply.github.com>
2020-10-22 23:46:19 +0700

    assign any user to role

--- console/migrations/m201022_150734_create_assign_any_user_to_role.php
+++ console/migrations/m201022_150734_create_assign_any_user_to_role.php
@@ -15,9 +15,11 @@ class m201022_150734_create_assign_any_user_to_role extends Migration
     {
         $auth = Yii::$app->authManager;
         $auth->init();
-        $author = $auth->getRole('author');
-        $admin= $auth->getRole('admin');
-        $superadmin= $auth->getRole('super-admin');
+
+        $author=$auth->getRole('author');
+        $admin=$auth->getRole('admin');
+        $superadmin=$auth->getRole('super-admin');
+
         $auth->assign($author, 1);
         $auth->assign($admin, 2);
         $auth->assign($superadmin, 3);
@@ -32,9 +34,10 @@ class m201022_150734_create_assign_any_user_to_role extends Migration
     {
         $auth = Yii::$app->authManager;
         $auth->init();
-        $author = $auth->getRole('author');
-        $admin= $auth->getRole('admin');
-        $superadmin= $auth->getRole('super-admin');
+
+        $author =$auth->getRole('author');
+        $admin=$auth->getRole('admin');
+        $superadmin=$auth->getRole('super-admin');
 
         $auth->revoke($author, 1);
         $auth->revoke($admin, 2);
